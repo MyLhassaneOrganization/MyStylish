932c2be44ca9c4cbec765890c14d8cfd8c9b79e6
mastinoz <persiste1@gmail.com>
2019-11-11 07:39:47 +0100

    Added some commrnts

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+// Change the background and font color for all websiyes
 let body = document.querySelectorAll('.c1,header,section,p,main,html,input,body,div,pre,code,a,ul,li,blockquote,textarea');
 for (elt of body) {
     elt.style.setProperty('background', '#71968a', 'important');
@@ -10,27 +11,24 @@ for (elt of html) {
 
 // Goole Goole Goole Goole Goole Goole Goole Goole Goole Goole Goole
 
+// Alittle manipulatation in the google results page
 let isValidHostname = /google.com\\?/.test(window.location);
 if (isValidHostname){
 
         let div = document.getElementsByTagName('div');
     for (elt of div) {
-        elt.style['color'] = '#667b66';
         elt.style.setProperty('color', '#667b66', 'important');
     }
     let a = document.getElementsByTagName('a');
     for (elt of a) {
-        elt.style['color'] = '#515151';
         elt.style.setProperty('color', '#515151', 'important');
     }
     let cite = document.getElementsByTagName('Cite');
     for (elt of cite) {
-        elt.style['color'] = '#bebebe';
         elt.style.setProperty('color', '#bebebe', 'important');
     }
     let em = document.getElementsByTagName('em');
     for (elt of em) {
-        elt.style['color'] = '#d2d2d2';
         elt.style.setProperty('color', '#d2d2d2', 'important');
     }
 }
